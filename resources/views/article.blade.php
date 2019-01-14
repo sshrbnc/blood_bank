76a54a40ded10e0f5235fa48b16464c23f653e08
Shaira <abancioshaira1@gmail.com>
2019-01-14 22:48:58 +0800

    updated article.blade.php

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -2,12 +2,8 @@
 
 @section('main')
 
-
-
-
-
 <div class="col-lg-10 col-xl-8 offset-lg-1 offset-xl-2">
-    <h2 class="text-center">রক্ত দিন জীবন বাঁচান , এক ব্যাগ রক্ত পারে একটি জীবন বাঁচাতে</h2>
+    <h2 class="text-center">CHARAN!</h2>
     <img class="img-fluid" src="assets/img/Blood-Bank.jpg">
     <p>
 
@@ -15,9 +11,5 @@
 </div>
 
 </div>
-
-
-
-
 
 @endsection
